ae6872f6a02db3382bb14797e747c360a42e88cf
Mateusz Szczesny <mati9456@gmail.com>
2023-12-03 16:49:05 +0100

    added data storage bucket

--- modules/data-pipeline/main.tf
+++ modules/data-pipeline/main.tf
@@ -22,9 +22,27 @@ resource "google_storage_bucket" "tbd-code-bucket" {
   public_access_prevention = "enforced"
 }
 
+resource "google_storage_bucket" "tbd-data-bucket" {
+  project                     = var.project_name
+  name                        = var.data_bucket_name
+  location                    = var.region
+  uniform_bucket_level_access = false #tfsec:ignore:google-storage-enable-ubla
+  force_destroy               = true
+  #checkov:skip=CKV_GCP_62: "Bucket should log access"
+  #checkov:skip=CKV_GCP_29: "Ensure that Cloud Storage buckets have uniform bucket-level access enabled"
+  #checkov:skip=CKV_GCP_78: "Ensure Cloud storage has versioning enabled"
+  public_access_prevention    = "enforced"
+}
+
 resource "google_storage_bucket_iam_member" "tbd-code-bucket-iam-viewer" {
   bucket = google_storage_bucket.tbd-code-bucket.name
   role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${var.data_service_account}"
+}
+
+resource "google_storage_bucket_iam_member" "tbd-data-bucket-iam-editor" {
+  bucket = google_storage_bucket.tbd-data-bucket.name
+  role   = "roles/storage.objectUser"
   member = "serviceAccount:${var.data_service_account}"
 }
 
